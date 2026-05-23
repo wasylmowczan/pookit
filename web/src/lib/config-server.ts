f183b4ce58656afca1936fdc6e181b9b1bfa6fe7
Wasyl Mowczan <mowczanwasyl@gmail.com>
2026-05-23 22:41:28 +0200

    fix: update environment variables in .env.example and config-server.ts for PocketBase integration

--- web/src/lib/config-server.ts
+++ web/src/lib/config-server.ts
@@ -1,9 +1,6 @@
 import { env } from '$env/dynamic/private';
 
 export const config = {
-	pocketbaseUrl: env.POCKETBASE_URL,
-	pocketbaseAdminEmail: env.POCKETBASE_ADMIN_EMAIL,
-	pocketbaseAdminPassword: env.POCKETBASE_ADMIN_PASSWORD,
 	posthogApiKey: env.PRIVATE_POSTHOG_PROJECT_API_KEY,
 	posthogApiHost: env.PRIVATE_POSTHOG_API_HOST
 };
