9960840ef885dbe91617545977eb23d6417a8cdc
Wasyl Mowczan <mowczanwasyl@gmail.com>
2026-05-23 22:19:22 +0200

    fix: update GitHub button link to point to the correct repository

--- web/src/lib/components/pages/LandingPage/components/HeroSection/index.svelte
+++ web/src/lib/components/pages/LandingPage/components/HeroSection/index.svelte
@@ -36,7 +36,7 @@
 
 		<div class="flex gap-4">
 			<Button size="lg" href="/register">Start building</Button>
-			<Button size="lg" href="https://github.com" variant="outline" target="_blank">
+			<Button size="lg" href="https://github.com/wasylmowczan/svelte.rocks" variant="outline" target="_blank">
 				<GitHubIcon/> GitHub
 			</Button>
 		</div>
