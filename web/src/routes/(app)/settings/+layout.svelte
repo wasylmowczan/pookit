6a42440206aa25b2038b63733f56eb5e6c9baa75
Wasyl Mowczan <mowczanwasyl@gmail.com>
2026-06-09 22:12:06 +0200

    fix: fix delete button text color

--- web/src/routes/(app)/settings/+layout.svelte
+++ web/src/routes/(app)/settings/+layout.svelte
@@ -47,8 +47,8 @@
 					class={cn(
 						isActive && 'bg-muted-foreground/5',
 						danger
-							? 'flex items-center gap-3 rounded-lg px-3 py-2 text-destructive transition-all hover:text-destructive/80'
-							: 'flex items-center gap-3 rounded-lg px-3 py-2 transition-all hover:text-primary'
+							? 'flex items-center gap-3 rounded-lg px-3 py-2 text-destructive transition-all hover:text-destructive/80 hover:text-primary'
+							: 'flex items-center gap-3 rounded-lg px-3 py-2 transition-all'
 					)}
 				>
 					{#if icon}
